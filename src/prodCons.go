dd10f809f8e1b7713527ad6e7114c9391449a0ce
janinamass <janina.mass@hhu.de>
2014-06-30 10:48:15 +0200

    clean up

--- src/prodCons.go
+++ src/prodCons.go
@@ -1,2 +1,51 @@
-package main
 
+
+package main
+/* producer-consumer problem in Go */
+
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var done = make(chan bool)
+var msgs = make(chan int)
+func produce () {
+	for i := 0; i < 10000000; i++ {
+		msgs <- i
+	}
+	done <- true
+}
+
+func consume () {
+	for {
+		msg := <-msgs
+		fmt.Println(msg)
+	}
+}
+func consume2 () {
+	for {
+		msg := <-msgs
+		fmt.Println(msg)
+	}
+}
+func Consumer(limit int){
+	for i:=0;i<limit;i++{
+		go consume()
+	}
+}
+func Producer(limit int){
+	for i:=0;i<limit;i++{
+		go produce()
+	}
+}
+
+func main () {
+	runtime.GOMAXPROCS(4)
+	//go produce()
+	Producer(4)
+	Consumer(4)
+	//go consume()
+	<- done
+}
